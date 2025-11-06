9a72dd9a49d21cca078a06f31307662d1c23d7f5
Martin <Martin.weiss2410@gmail.com>
2025-11-06 17:52:25 +0700

    added the old final rpg

---
+++ Final RPG game/Assets/Scripts/Scene Scripts/SceneChanger.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string sceneToLoad;
+    public float fadeTime = 0.5f;
+    public Animator fadeAnim;
+    public Vector2 newPlayerPosition;
+    private Transform player;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            player = collision.transform;
+            StartCoroutine(FadeAndLoad());
+        }
+    }
+
+    IEnumerator FadeAndLoad()
+    {
+        fadeAnim.SetTrigger("FadeAwayAnimation"); 
+        yield return new WaitForSeconds(fadeTime);
+
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
